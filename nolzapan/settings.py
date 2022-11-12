7d4cbf72f9193d6274d5cfe73a845fb181173da2
aka-shiny <aka.shiny.dev@email.com>
2022-11-13 00:58:48 +0900

    urls.py, settings.py 수정

--- nolzapan/settings.py
+++ nolzapan/settings.py
@@ -18,7 +18,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "blog.apps.BlogConfig"
+    "blog.apps.BlogConfig",
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,7 @@ WSGI_APPLICATION = "nolzapan.wsgi.application"
 
 DATABASES = local_settings.DATABASES_SQLITE_NOLZAPAN
 
+AUTH_USER_MODEL = "users.User"
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
